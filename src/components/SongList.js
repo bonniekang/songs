74de4c0834a89d47ea1b5e503d86950e20de015a
jihye kang <jihye59kang@gmail.com>
2021-07-13 23:38:58 +0100

    rendering song list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div>
+                    {song.title}
+                </div>
+            )
+        })
+    }
+
     render() {
         return (
             <div>
-                SongList
+                {this.renderList()}
             </div>
         )
     }
