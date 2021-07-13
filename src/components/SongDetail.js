5f9fc4894319f00f787f663de95a52427695811d
jihye kang <jihye59kang@gmail.com>
2021-07-14 00:54:57 +0100

    add condition

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
+const SongDetail = ({song}) => {
+    if (!song) {
+        return <div>select song</div>
+    }
     return (
         <div>
-            {props.song}
+            {song.title}
+            {song.duration}
         </div>
     )
 }
